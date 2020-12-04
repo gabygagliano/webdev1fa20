84ef8944086eb3cbbc7365c6d819a6950ffc5e99
gabygagliano <71103379+gabygagliano@users.noreply.github.com>
2020-12-03 21:43:34 -0600

    im learning finally

---
+++ class_code/jquery/main.js
@@ -0,0 +1,10 @@
+$('.box').click(function(){
+    console.log("i was clicked!")
+    $('.box').css("background", "#aaffaa");
+})
+
+function addSomenumberssure() {
+
+    console.log(1+8);
+    console.log("i added some numbers what more do you want from me?")
+}
