d1e2365d3ef2da6587d80341812a9ca7c994d551
gabygagliano <71103379+gabygagliano@users.noreply.github.com>
2020-12-11 11:25:29 -0600

    yo

---
+++ css_portfolio/portfolioweek7/jquery_modal/main.js
@@ -0,0 +1,13 @@
+$('#show-modal').click(function(){
+    $('#modal').show()
+    $('#box-hide').hide();
+    $('#box-toggle').toggle();
+  })
+  
+  $('#hide').click(function(){
+    
+  })
+  
+  $('#toggle').click(function(){
+    
+  })
